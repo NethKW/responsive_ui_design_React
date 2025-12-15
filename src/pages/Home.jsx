8229448fec6259329ad86a096c0c7de03a2639c3
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-15 20:29:56 +0530

    create and update Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Body from "../components/Body";
+import Header from "../components/Header";
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
