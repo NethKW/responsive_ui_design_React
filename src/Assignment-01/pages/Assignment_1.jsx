612a76b1ee1eec1074ef99f949e21f862d583b4e
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-17 12:31:45 +0530

    rename the Home page

---
+++ src/Assignment-01/pages/Assignment_1.jsx
@@ -0,0 +1,10 @@
+import Body from "../components/Body";
+import Header from "../components/Header";
+export default function Assignment_1() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
